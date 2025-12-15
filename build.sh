f5769a36ab9db4583f4b748eaec8a870ac7a1a5b
SorerBOT <alonfiller@gmail.com>
2025-12-15 10:49:59 +0200

    (Feat) no longer uploading binaries

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 C_VERSION="c99"
 FLAGS="-Iexternal -Wall -Wextra -gdwarf-4 -O0 -lm"
-clang -std=$C_VERSION $FLAGS nn_xor.c -o nn_xor
-clang -std=$C_VERSION $FLAGS nn_mul.c -o nn_mul
-clang -std=$C_VERSION $FLAGS nn_back_propagation_test.c -o nn_back_propagation_test
-clang -std=$C_VERSION $FLAGS nn_images.c -o nn_images
+clang -std=$C_VERSION $FLAGS nn_xor.c -o nn_xor.o
+clang -std=$C_VERSION $FLAGS nn_mul.c -o nn_mul.o
+clang -std=$C_VERSION $FLAGS nn_back_propagation_test.c -o nn_back_propagation_test.o
+clang -std=$C_VERSION $FLAGS nn_images.c -o nn_images.o
